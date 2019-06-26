94539c13137e20f4a2673ef9607cefa29cbe004a
王志杰 <1327342025@qq.com>
2019-06-26 16:16:26 +0800

    题目2

---
+++ src/main/java/ListNode.java
@@ -0,0 +1,12 @@
+/**
+ * @author Wangzj
+ * @date 2019/6/26 15:20
+ */
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+
+}
---
+++ src/main/java/Solution2.java
@@ -0,0 +1,38 @@
+/**
+ * 没看明白题目
+ * @author Wangzj
+ * @date 2019/6/26 15:18
+ */
+public class Solution2 {
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            System.out.println(sum);
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(342);
+        ListNode l2 = new ListNode(465);
+        addTwoNumbers(l1,l2);
+    }
+}
