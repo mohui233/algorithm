8a88e33c8a7204212a12abb1f1f25fa4cc251ae2
王志杰 <1327342025@qq.com>
2019-06-27 15:25:53 +0800

    题目3

--- src/main/java/Solution3.java
+++ src/main/java/Solution3.java
@@ -1,25 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Wangzj
  * @date 2019/6/26 16:17
  */
 public class Solution3 {
     public static int lengthOfLongestSubstring(String s) {
-        String[] a = new String[s.length()];
-        char[] b = s.toCharArray();
-        StringBuilder e = new StringBuilder();
-        for (int i=0; i<b.length; i++){
-            char f = b[i];
-            String c = String.valueOf(f);
-            String k = e.toString();
-            if (s.contains(c)){
-                e.append(c);
-                System.out.println(k);
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < n; end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha), start);
             }
+            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end + 1);
         }
-        return 0;
+        return ans;
     }
 
     public static void main(String[] args) {
-        lengthOfLongestSubstring("abbbcdbe");
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
     }
 }
